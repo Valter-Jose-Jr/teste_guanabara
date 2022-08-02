584485becb2666867730993dd155677d0ca1a288
valter jose <programando.so@gmail.com>
2022-08-02 11:37:27 -0300

    testando branch
    fiz algumas alterações para ver como fica neste caso ciei um brach de nome conteúdo.

--- branch.dart
+++ branch.dart
@@ -1,3 +1,4 @@
 void main() {
-  print("Aula de branch git github");
+  print("Aula de branch git github\n");
+  print("Foi criado do novos métodos um com nome conteúdo e outro com noome design");
 }
